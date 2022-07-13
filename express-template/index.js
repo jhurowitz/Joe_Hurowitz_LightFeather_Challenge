3aaaaaba1b533d3ed0280b3613305c9c079685f9
jhurowitz <joseph.hurowitz@gmail.com>
2022-07-13 01:21:32 -0400

    Final Attempt
    Could not get post method to work. Validations seemed to work on FireFox, but not on Chrome. Form CSS was found on website that is mentioned on style sheet. Get method (api/supervisors) works and auto-populates drop down.

--- express-template/index.js
+++ express-template/index.js
@@ -1,63 +1,103 @@
 const express = require('express')
 const request = require('request')
 const cors = require('cors');
+const { application, response } = require('express');
+const bodyParser = require('body-parser');
+const e = require('express');
 
-const app = new express()
+const app = new express();
 app.use(express.json())
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/api/supervisors', async (req, res) => {
-    var url = 'https://o3m5qixdng.execute-api.us-east-1.amazonaws.com/api/managers';
-    request({
-        url: url,
-        json: true
-    }, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            
-            // Build JSON
-            var supervisors = []
-            for (var id in body)
-            {
-                // If jurisdiction is not a number, include supervisor
-                var supervisor = body[id]
-                if (isNaN(supervisor.jurisdiction)) {
-                    var string = `${supervisor.jurisdiction} - ${supervisor.lastName}, ${supervisor.firstName}`
-                    supervisors.push(string)
-                }
+    const supervisors = []
+    fetch('https://o3m5qixdng.execute-api.us-east-1.amazonaws.com/api/managers')
+      .then((response) => response.json())
+      .then((data) => {
+        for (var id in data) {
+            var supervisor = data[id]
+            if (isNaN(supervisor.jurisdiction)) {
+                var string = `${supervisor.jurisdiction} - ${supervisor.lastName}, ${supervisor.firstName}`
+                supervisors.push(string)
             }
-
-            // Sort by jurisdiction, last name, then first name
-            supervisors.sort((a, b) => {
-                
-                // Array containing [jurisdiction, last name, first name] 
-                aProps = [a[0], a.substring(a.indexOf('-') + 2, a.indexOf(',') - 1), 
-                    a.substring(a.indexOf(',') + 2)]
-                bProps = [b[0], b.substring(b.indexOf('-') + 2, b.indexOf(',') - 1), 
-                    b.substring(b.indexOf(',') + 2)]
-                
-                // If first in alphabet, return -1 and move up
-                // If second in alphabet, return 1 and move down
-                // If equal, move to next sort
-                if (aProps[0].localeCompare(bProps[0]) < 0) return -1;
-                if (aProps[0].localeCompare(bProps[0]) > 0) return 1;
-
-                if (aProps[1].localeCompare(bProps[1]) < 0) return -1;
-                if (aProps[1].localeCompare(bProps[1]) > 0) return 1;
-
-                if (aProps[2].localeCompare(bProps[2]) < 0) return -1;
-                if (aProps[2].localeCompare(bProps[2]) > 0) return 1;
-            })
-
-            res.json(supervisors)
         }
+ 
+        supervisors.sort((a, b) => {  
+            aProps = [a[0], a.substring(a.indexOf('-') + 2, a.indexOf(',') - 1), 
+                a.substring(a.indexOf(',') + 2)]
+            bProps = [b[0], b.substring(b.indexOf('-') + 2, b.indexOf(',') - 1), 
+                b.substring(b.indexOf(',') + 2)]
+                    
+            if (aProps[0].localeCompare(bProps[0]) < 0) return -1;
+            if (aProps[0].localeCompare(bProps[0]) > 0) return 1;
+            
+            if (aProps[1].localeCompare(bProps[1]) < 0) return -1;
+            if (aProps[1].localeCompare(bProps[1]) > 0) return 1;
+            
+            if (aProps[2].localeCompare(bProps[2]) < 0) return -1;
+            if (aProps[2].localeCompare(bProps[2]) > 0) return 1;
+        })
+
+        res.json(supervisors)
     })
 })
 
 app.post('/api/submit', async (req, res) => {
     try {
-        res.json({
-            'body': req.body
-        })
+        var validForm = true
+
+        // Validate first name
+        if (req.body.firstName != null && req.body.firstName.length > 0) {
+            if (/\d+/.test(req.body.firstName)) {
+                throw new Error("Invalid first name, can only contain letters.")
+                validForm = false
+            } 
+        } 
+        else {
+            throw new Error("First name is missing.")
+        }
+
+        // Validate last name
+        if (req.body.lastName != null && req.body.lastName.length > 0) {
+            if (/\d+/.test(req.body.lastName)) {
+                throw new Error("Invalid last name, can only contain letters.")
+                validForm = false
+            } 
+        } 
+        else {
+            throw new Error("Last name is missing.")
+            validForm = false
+        }
+
+        // Validate supervisor
+        if (req.body.supervisor != null && req.body.supervisor.length > 0) {
+            throw new Error("Supervisor is missing.")
+            validForm = false
+        }
+
+        // Validate phone number
+        if (mismatch(reg.body.phone, /^\+?\d?[ -]?\(?\d{3}\)?[ -]?\d{3}[ -]?\d{4}$/)) {
+            throw new Error("Invalid phone number.")
+            validForm = false
+        }
+
+        // Validate email
+        if (mismatch(reg.body.email.toLowerCase(), /^[a-z0-9\-_\.]+@[a-z0-9\-_\.]+\.[a-z]{2,4}$/)) {
+            throw new Error("Invalid email.")
+            validForm = false
+        }
+
+        if (validForm) {
+            console.log({
+                'First Name' : req.body.firstName, 
+                'Last Name' : req.body.lastName, 
+                'Phone' : req.body.phone, 
+                'Email' : req.body.email, 
+                'Supervisor' : req.body.supervisor
+            })
+        }
+
     }
     catch(e) {
         res.json({
--- react-template/src/App.js
+++ react-template/src/App.js
@@ -1,54 +1,140 @@
+import React, { useEffect } from 'react';
 import './App.css';
+import { useState } from "react";
 
 function App() {
-  // const supervisors = [
-  //   { supervisor: "Smith, John" },
-  //   { supervisor: "Doe, John" },
-  //   { supervisor: "Jane, John" }
-  // ]
+  
+  const [supervisors, setSupervisors] = useState([]);
+  const [supervisor, setSupervisor] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [emailCheckBoxChecked, setEmailCheckBox] = useState(true);
+  const [phoneCheckBoxChecked, setPhoneCheckBox] = useState(true);
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
 
-  // const supervisors = []
-  // fetch('http://localhost:8080/api/supervisors')
-  // .then((response) => response.json())
-  // .then((supervisor) => {
-  //   supervisors.push(supervisor)
-  // })
+  useEffect(() => {
+      fetch('http://localhost:8080/api/supervisors')
+      .then((response) => response.json())
+      .then((supervisor) => { 
+        setSupervisors(supervisor)
+      })
+  })
 
-  fetch('http://localhost:8080/api/supervisors')
-    .then((response) => response.json())
-    .then((supervisor) => {
-      console.log('Fetched: ' + supervisor)  
-      return (
-        <div></div>
-      )
+  const handleFirstNameChange = (e) => {
+      setFirstName(e.target.value);
+  }
+
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value);
+  }
+
+  var handleEmailCheckBox = (e) => {
+    if (emailCheckBoxChecked) {
+      setEmailCheckBox(false)
+    } 
+    else {
+      setEmailCheckBox(true)
+    }
+  }
+
+  var handlePhoneCheckBox = (e) => {
+    if (phoneCheckBoxChecked) {
+      setPhoneCheckBox(false)
+    } 
+    else {
+      setPhoneCheckBox(true)
+    }
+  }
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const handlePhoneNumberChange = (e) => {
+    setPhone(e.target.value);
+  }
+  
+  const handleSupervisorChange = (e) => {
+    setSupervisor(e.target.value);
+  }
+
+  const handleFormSubmit = (e) => {
+    console.log(JSON.stringify({ firstName, lastName, email, phone, supervisor }))
+    fetch('http://localhost:8080/api/submit', {
+      method : 'POST',
+      headers : { 'Content-type' : 'application/json; charset=UTF-8' }, 
+      body : JSON.stringify({ firstName })
     })
+    .then((response) => response.json())
+    .then((data) => alert(data))
+  }
 
   return (
-    <div className="App">
-      <form>
-        <label>First Name</label>
-        <input placeholder='First Name'/>
-        <br/>
-        <label>Last Name</label>
-        <input placeholder='Last Name'/>
-        <br/>
-        <label>Email</label>
-        <input placeholder='Email'/>
-        <br/>
-        <label>Phone Number</label>
-        <input placeholder='Phone Number'/>
-        <br/>
-        <label>Supervisors</label>
-        <select>
-          {/* {
-            supervisors.map((option) => {
-              <option value={option.supervisor}></option>
-            })
-          } */}
+    <div className='App'>
+      <div className='form-style-2'>
+        <div className='form-style-2-heading'>Notification Form</div>
+        <form onSubmit={handleFormSubmit}>
+        <label htmlFor='firstName'><span className='required'>First Name *</span></label>
+        <input 
+          required
+          className='input-field'
+          type='text'
+          name='firstName'
+          onChange={handleFirstNameChange}
+          value={firstName}
+        /><br/>
+        <label htmlFor='lastName'><span className='required'>Last Name *</span></label>
+        <input 
+          required
+          className='input-field'
+          type='text'
+          name='lastName'
+          onChange={handleLastNameChange}
+          value={lastName}
+        /><br/>
+        <p>How would you prefer to be notified?</p>
+        <label htmlFor='email'><span>Email</span></label>
+        <input 
+          className='input-field'
+          type='email'
+          name='email'
+          onChange={handleEmailChange}
+          value={email}
+        />
+        <input 
+          type='checkbox'
+          name='emailCheckBox'
+          onChange={handleEmailCheckBox}
+          required={emailCheckBoxChecked}
+        />
+        <label htmlFor='phone'><span>Phone number</span></label>
+        <input 
+          className='input-field'
+          type='text'
+          name='phone'
+          onChange={handlePhoneNumberChange}
+          value={phone}
+        />
+        <input 
+          type='checkbox'
+          name='phoneNumberCheckBox'
+          onChange={handlePhoneCheckBox}
+          required={phoneCheckBoxChecked}
+        />
+        <label htmlFor='supervisorsDropDown'><span>Supervisors</span></label>
+        <select required className='select-field' name='supervisorsDropDown' onChange={handleSupervisorChange}>
+          <option key='baseOption' value={supervisor}></option>
+          { 
+            supervisors.map((sup) => { 
+              return <option text='text' key={sup} value={supervisor}>{sup}</option> 
+            }) 
+          }
         </select>
         <br/>
-        <button>Submit</button>
+        <label><span> </span><input type="submit" value="Submit"/></label>
       </form>
+      </div>
     </div>
   );
 }
